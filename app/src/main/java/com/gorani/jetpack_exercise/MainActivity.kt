c14c25eba9337baceeb31169fff6916f3e7ae78a
gorani16 <jgee0116@naver.com>
2023-02-15 03:43:31 +0900

    - DataBinding + Data Class Exercise

--- app/src/main/java/com/gorani/jetpack_exercise/MainActivity.kt
+++ app/src/main/java/com/gorani/jetpack_exercise/MainActivity.kt
@@ -10,16 +10,31 @@ import com.gorani.jetpack_exercise.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var ageCount = 16
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.activity = this@MainActivity
+        binding.person = Person("고라니", 16)
+
+        nextActivity()
+
+    }
+
+    private fun nextActivity() {
         binding.btnNextActivity.setOnClickListener {
             val intent = Intent(this, NextActivity::class.java)
             startActivity(intent)
             Toast.makeText(this, "다음 액티비티로 이동합니다.", Toast.LENGTH_SHORT).show()
         }
-
     }
+
+    fun plusAge() {
+        Toast.makeText(this, "버튼 클릭!", Toast.LENGTH_SHORT).show()
+        ageCount++
+        binding.person = Person("고라니", ageCount)
+    }
+
 }
---
+++ app/src/main/java/com/gorani/jetpack_exercise/Person.kt
@@ -0,0 +1,6 @@
+package com.gorani.jetpack_exercise
+
+data class Person(
+    val name: String,
+    val age: Int
+)
