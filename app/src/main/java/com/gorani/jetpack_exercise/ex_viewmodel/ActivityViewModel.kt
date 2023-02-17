038bdc24b07fff768c54a405eb4766d85d1080b1
gorani16 <jgee0116@naver.com>
2023-02-17 14:38:37 +0900

    - ViewModel, ViewModel Factory Description.

--- app/src/main/java/com/gorani/jetpack_exercise/ex_viewmodel/ActivityViewModel.kt
+++ app/src/main/java/com/gorani/jetpack_exercise/ex_viewmodel/ActivityViewModel.kt
@@ -17,11 +17,13 @@ class ActivityViewModel(private val num: Int) : ViewModel() {
     }
 
     /**
-     * ViewModel 에서는 데이터를 처리하는 로직을 구현한다.
+     * 일반적으로 비즈니스 로직은 Data Layer 에 있지만 UI Layer 에도 비즈니스 로직이 포함될 수 있다.
+     * ViewModel 에 포함되는 그 로직은 서버 또는 DB 로 부터 데이터를 ViewModel 로 받아오는 로직이다.
      * 데이터를 처리하는 로직을 구현해야하는 경우는 아래와 같이 두 가지로 구분된다.
      * 1. 네트워크 통신을 할 때, 즉 서버로부터 데이터를 가져올 때 => Repository -> Retrofit
      * 2. Local DB 로 부터 데이터를 가져올 때 => Room, SQLite
      * 이 때, Repository 나 Local DB 로 부터 ViewModel 로 데이터를 전달해야할 때 ViewModel Factory 를 사용한다.
+     * 참고 : https://developer.android.com/topic/libraries/architecture/viewmodel?hl=ko
      */
 
     fun plus() {
